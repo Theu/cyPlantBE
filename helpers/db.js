553a1356632734c34565277416e696ee05606b17
Theu <info@matteocontini.com>
2018-08-14 22:08:07 +0200

    created instanceOfDb with a singleton to check if db alredy exists else to create

---
+++ helpers/db.js
@@ -0,0 +1,11 @@
+import PouchDB from 'pouchdb';
+
+let instanceOfDb;
+
+export const getDB = () => {
+    if(!instanceOfDb) {
+        instanceOfDb = new PouchDB('newDB');
+    }
+
+    return instanceOfDb;
+}
