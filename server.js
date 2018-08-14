7b484283e1537e39d7e95adcb750e4f0cc5a80fd
Theu <info@matteocontini.com>
2018-08-14 22:02:43 +0200

    added logger, slow middlewares. added todo routes

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const Koa = require('koa');
+import Koa from 'koa';
+import bodyparser from 'koa-bodyparser';
+import logger from 'koa-logger';
+import slow from 'koa-slow';
+import todos from './routes/todos';
 
 const app = new Koa();
-
-app.use(async ctx => {
-    ctx.body = 'hello world'
-});
+app.use(bodyparser());
+app.use(logger());
+app.use(todos.routes());
 
 app.listen(3000);
